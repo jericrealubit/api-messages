592cb739837e441eab358e2c9ced8c729358629d
jeric realubit <jericrealubit@gmail.com>
2022-09-12 14:52:34 +1200

    first commit

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://jeric123:jeric123@cluster0.fkrqcuy.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  if (err) throw err; 
+  if (err) throw err;
 });
-const collection = client.db("store").collection("users");
+const collection = client.db("store").collection("messages");
 
 module.exports = { collection, ObjectId }
